e872df8df44a4162295f464508bd388f3ff445bb
xiaojianma9999 <630913782@qq.com>
2022-06-16 00:26:25 +0800

    【TicketNo:】20220616002 【Description:】【消费详情】界面时间格式开发 【Binary Source:】NA

--- app/src/main/java/com/xiaojianma/carconsumptionmanager/activity/ConsumeDetailsActivity.java
+++ app/src/main/java/com/xiaojianma/carconsumptionmanager/activity/ConsumeDetailsActivity.java
@@ -10,6 +10,7 @@ import com.xiaojianma.carconsumptionmanager.adapter.ConsumeDetailsAdapter;
 import com.xiaojianma.carconsumptionmanager.model.ConsumeItem;
 
 import java.math.BigDecimal;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -27,8 +28,9 @@ public class ConsumeDetailsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_consume_details);
         listView = findViewById(R.id.consume_details_list);
         List<ConsumeItem> list = new ArrayList<>(50);
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
         for (int i = 0; i < 50; i++) {
-            ConsumeItem item = new ConsumeItem("类型" + i, new BigDecimal(i * i), "position" + i, new Date().toString(), false);
+            ConsumeItem item = new ConsumeItem("类型" + i, new BigDecimal(i * i), "position" + i, format.format(new Date()), false);
             list.add(item);
         }
         ConsumeDetailsAdapter adapter = new ConsumeDetailsAdapter(this, list);
