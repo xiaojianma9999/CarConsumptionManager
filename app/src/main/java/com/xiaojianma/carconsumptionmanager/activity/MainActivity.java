76fb4568578888f25e26f82f81d434bde3f02650
xiaojianma9999 <630913782@qq.com>
2022-06-16 23:15:17 +0800

    【TicketNo:】20220616003 【Description:】【创建、更新消费】界面开发 【Binary Source:】NA

--- app/src/main/java/com/xiaojianma/carconsumptionmanager/activity/MainActivity.java
+++ app/src/main/java/com/xiaojianma/carconsumptionmanager/activity/MainActivity.java
@@ -139,7 +139,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-        Intent intent = new Intent(MainActivity.this, ConsumeDetailsActivity.class);
+        Intent intent = new Intent(MainActivity.this, ConsumeCreateActivity.class);
         startActivity(intent);
     }
 }
